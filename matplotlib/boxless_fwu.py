81c5bbff4df5cd6b900c5dc8baa72bfcda7c49be
Hannah Winter <chmuca@alumni.uv.es>
2022-04-24 13:16:27 +0200

    edit matplotlib

--- matplotlib/boxless_fwu.py
+++ matplotlib/boxless_fwu.py
@@ -1,3 +1,4 @@
+from matplotlib import cm                   # Colour gradient (colormap)
 import matplotlib.pyplot as plt             # Generate plots
 from mpl_toolkits.mplot3d import Axes3D     # Have a 3D env
 
@@ -12,12 +13,13 @@ x,y = np.meshgrid(interval,interval)
 
 # Define the parametric equations
 X=x
-Y=y*y       # Alternative: np.power(y,2)
+Y=3*np       # Alternative: np.power(y,2)
 Z=x*y*y*y   # Alternative: x*np.power(y,3)
 
 fig = plt.figure()      # Generate a figure called "fig"
 
 ax = fig.add_subplot(projection='3d')
 ax.axis('off')          # Hide the "background box"
-ax.plot_surface(X,Y,Z)  # Draw solid surface (will not show)
+ax.plot_surface(X,Z,Y, cmap=cm.ocean)
 fig.show()              # Show "fig"
+fig.savefig('folded_umbrella.eps')
